b94f0774a4a0b66e4a98b5a212f4b143f50cbf3c
divyanshrai <divyansh.rai.1999@gmail.com>
2020-07-27 10:43:23 +0530

    Added a red colour to errors and added name validator to model

--- main/forms.py
+++ main/forms.py
@@ -20,24 +20,3 @@ class RegisterForm(forms.ModelForm):
                 'student_email',
                 'student_img',
                 'student_DOB')
-    
-    def clean_student_name(self):
-        student_name = self.cleaned_data['student_name']
-        #### checking if name field has digits ####
-        if checkfordigits(student_name):
-            raise ValidationError('Name cannot contain a digit')
-
-        return student_name
-        
-    
-''' Methods that help validate the fields
-checkfordigits(s) - Checks each letter for digits
-
-
- '''
-
-def checkfordigits(s):
-    for character in s:
-        if character.isdigit():
-            return True
-    return False
--- main/models.py
+++ main/models.py
@@ -4,14 +4,16 @@ from datetime import datetime,date
 from django.utils import timezone as tz
 from django.forms import ValidationError
 
-''' Methods that help validate the fields
+''' Validators Methods that help validate the fields
 checkfordigits(s) - Checks each letter for digits
 no_Future_Dob(value) - Ensures DOB is not in the future
 
 
  '''
-
-
+def checkForDigits(value):
+    for character in value:
+        if character.isdigit():
+            raise ValidationError('Name cannot contain a digit.')
 
 def no_Future_Dob(value):
     today = date.today()
@@ -21,14 +23,21 @@ def no_Future_Dob(value):
 
 # Create your models here.
 class student(models.Model):
-    student_name=models.CharField(max_length=100)
+    student_name=models.CharField(max_length=100,
+                                validators=[checkForDigits])
+
     student_age=models.IntegerField(validators=[MaxValueValidator(200),
                                     MinValueValidator(1)])
+                                    
     student_email=models.CharField(max_length=200)
+
     student_DOB=models.DateField("Enter Date of Birth of Student",
                                 default=datetime.now,
-                                validators=[no_Future_Dob])
+                                validators=[no_Future_Dob],
+                                help_text="Use the button on the right or the arrow keys to edit the date in DD-MM-YYYY format")
+
     student_timeofreg=models.DateTimeField(default=datetime.now)
+
     student_img=models.ImageField("Upload an image of the student",
                                 blank=True,upload_to="images/",
                                 default='default/blank_user.png')
