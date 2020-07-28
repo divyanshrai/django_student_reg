05ecd22a292ee3d12441367358ff9aedbad23ae3
divyanshrai <divyansh.rai.1999@gmail.com>
2020-07-28 14:04:01 +0530

    Code added to display a sorted merit list. Has to be made efficient later

--- main/views.py
+++ main/views.py
@@ -34,6 +34,20 @@ def registerstu(request):
     context={'form':form})
 
 def merit_list(request):
+    students=student.objects.all()
+    merit_dict={}
+    marka_dict={}
+    markb_dict={}
+    markc_dict={}
+    for stu in students:
+        stu_mark=stu.student_marks_A+stu.student_marks_B+stu.student_marks_C
+        merit_dict[stu.student_name]=stu_mark
+        marka_dict[stu.student_name]=stu.student_marks_A
+        markb_dict[stu.student_name]=stu.student_marks_B
+        markc_dict[stu.student_name]=stu.student_marks_C
+ #   merit_list=sorted(merit_dict.items(), key=lambda x: x[1],reverse=True)
+    merit_list=[(x,y,marka_dict[x],markb_dict[x],markc_dict[x]) for (x,y) in sorted(merit_dict.items(), key=lambda x: x[1],reverse=True)]
+
     return render(request,
     template_name='main/merit_list.html',
-    context={'students':student.objects.all})
+    context={'meritlist':merit_list})
